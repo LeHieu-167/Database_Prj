bdba7c868cbe9c0a709d9e0ddef6b1c3d12080ff
nguyenduchoang2907 <135149449+nguyenduchoang2907@users.noreply.github.com>
2023-07-12 13:38:53 +0700

    Update testdb_2.sql
    bản n chắc ổn

--- testdb_3.sql
+++ testdb_3.sql
@@ -1,13 +1,5 @@
--- Create the database
-CREATE DATABASE testdb;
-
--- Connect to the database
-\c testdb
-
--- Create the gender enum type
 CREATE TYPE gender AS ENUM ('Male', 'Female', 'Other');
 
--- Create the inform table
 CREATE TABLE inform (
   mssv INTEGER PRIMARY KEY,
   name VARCHAR(100),
@@ -19,7 +11,6 @@ CREATE TABLE inform (
   email VARCHAR(100)
 );
 
--- Create the achievement table
 CREATE TABLE achievement (
   mssv INTEGER PRIMARY KEY REFERENCES inform(mssv),
   cpa DECIMAL,
@@ -29,7 +20,6 @@ CREATE TABLE achievement (
   internship_experience TEXT
 );
 
--- Create the company table
 CREATE TABLE company (
   company_id SERIAL PRIMARY KEY,
   name VARCHAR(100),
@@ -40,7 +30,6 @@ CREATE TABLE company (
   description TEXT
 );
 
--- Create the position table
 CREATE TABLE position (
   position_id SERIAL PRIMARY KEY,
   company_id INTEGER REFERENCES company(company_id),
@@ -49,7 +38,6 @@ CREATE TABLE position (
   is_internship BOOLEAN
 );
 
--- Create the application table
 CREATE TABLE application (
   application_id SERIAL PRIMARY KEY,
   student_id INTEGER REFERENCES inform(mssv),
@@ -57,80 +45,51 @@ CREATE TABLE application (
   application_date DATE
 );
 
--- Insert random data into inform table
+
+
+
+-- Chèn dữ liệu vào bảng inform
 INSERT INTO inform (mssv, name, gender, specialize, graduation_year, github, facebook, email)
 VALUES
-  (1, 'John Doe', 'Male', 'Computer Science', 2022, 'github.com/johndoe', 'facebook.com/johndoe', 'johndoe@example.com'),
-  (2, 'Jane Smith', 'Female', 'Information Technology', 2023, 'github.com/janesmith', 'facebook.com/janesmith', 'janesmith@example.com'),
-  (3, 'Alex Johnson', 'Other', 'Software Engineering', 2021, 'github.com/alexjohnson', 'facebook.com/alexjohnson', 'alexjohnson@example.com'),
-  (4, 'Emily Brown', 'Female', 'Computer Science', 2022, 'github.com/emilybrown', 'facebook.com/emilybrown', 'emilybrown@example.com'),
-  (5, 'Michael Johnson', 'Male', 'Information Technology', 2023, 'github.com/michaeljohnson', 'facebook.com/michaeljohnson', 'michaeljohnson@example.com'),
-  (6, 'Sophia Lee', 'Female', 'Software Engineering', 2021, 'github.com/sophialee', 'facebook.com/sophialee', 'sophialee@example.com'),
-  (7, 'David Wilson', 'Male', 'Computer Science', 2022, 'github.com/davidwilson', 'facebook.com/davidwilson', 'davidwilson@example.com'),
-  (8, 'Olivia Davis', 'Female', 'Information Technology', 2023, 'github.com/oliviadavis', 'facebook.com/oliviadavis', 'oliviadavis@example.com'),
-  (9, 'Daniel Anderson', 'Male', 'Software Engineering', 2021, 'github.com/danielanderson', 'facebook.com/danielanderson', 'danielanderson@example.com');
+  (20180001, 'John Doe', 'Male', 'Computer Science', 2022, 'github.com/johndoe', 'facebook.com/johndoe', 'johndoe@example.com'),
+  (20180002, 'Emma Johnson', 'Female', 'Business Administration', 2023, 'github.com/emmajohnson', 'facebook.com/emmajohnson', 'emmajohnson@example.com'),
+  (20180003, 'Michael Brown', 'Male', 'Electrical Engineering', 2022, 'github.com/michaelbrown', 'facebook.com/michaelbrown', 'michaelbrown@example.com'),
+  (20190001, 'Sophia Wilson', 'Female', 'Chemical Engineering', 2022, 'github.com/sophiawilson', 'facebook.com/sophiawilson', 'sophiawilson@example.com'),
+  (20190002, 'Daniel Martinez', 'Male', 'Environmental Engineering', 2023, 'github.com/danielmartinez', 'facebook.com/danielmartinez', 'danielmartinez@example.com'),
+  (20220001, 'Olivia Anderson', 'Male', 'Mechanical Engineering', 2025, 'github.com/oliviaanderson', 'facebook.com/oliviaanderson', 'oliviaanderson@example.com'),
+  (20220002, 'Ava Thompson', 'Female', 'Business Administration', 2025, 'github.com/avathompson', 'facebook.com/avathompson', 'avathompson@example.com'),
+  (20230001, 'James Lee', 'Male', 'Electronics Engineering', 2027, 'github.com/jameslee', 'facebook.com/jameslee', 'jameslee@example.com'),
+  (20230002, 'Sophie Clark', 'Female', 'Environmental Engineering', 2027, 'github.com/sophieclark', 'facebook.com/sophieclark', 'sophieclark@example.com'),
+  (20230003, 'Matthew Wright', 'Male', 'Civil Engineering', 2026, 'github.com/matthewwright', 'facebook.com/matthewwright', 'matthewwright@example.com'),
+  (20220004, 'Isabella Turner', 'Female', 'Economics', 2026, 'github.com/isabellaturner', 'facebook.com/isabellaturner', 'isabellaturner@example.com'),
+  (20230005, 'Andrew Phillips', 'Male', 'Computer Science', 2027, 'github.com/andrewphillips', 'facebook.com/andrewphillips', 'andrewphillips@example.com'),
+  (20230006, 'Emily Rodriguez', 'Female', 'Business Administration', 2027, 'github.com/emilyrodriguez', 'facebook.com/emilyrodriguez', 'emilyrodriguez@example.com'),
+  (20210007, 'William Scott', 'Male', 'Electronics Engineering', 2026, 'github.com/williamscott', 'facebook.com/williamscott', 'williamscott@example.com'),
+  (20220008, 'Grace Lewis', 'Female', 'Environmental Engineering', 2026, 'github.com/gracelewis', 'facebook.com/gracelewis', 'gracelewis@example.com'),
+  (20230009, 'Victoria Turner', 'Female', 'Computer Science', 2027, 'github.com/victoriaturner', 'facebook.com/victoriaturner', 'victoriaturner@example.com'),
+  (20230010, 'David Johnson', 'Male', 'Electronics Engineering', 2026, 'github.com/davidjohnson', 'facebook.com/davidjohnson', 'davidjohnson@example.com'),
+  (20200011, 'Sophia White', 'Female', 'Environmental Engineering', 2024, 'github.com/sophiawhite', 'facebook.com/sophiawhite', 'sophiawhite@example.com'),
+  (20200002, 'John Doe', 'Male', 'Business Administration', 2024, 'github.com/johndoe2', 'facebook.com/johndoe2', 'johndoe2@example.com');
 
--- Insert random data into achievement table
+
 INSERT INTO achievement (mssv, cpa, programming_lang, language_cert, project, internship_experience)
 VALUES
-  (1, 3.8, 'Python, Java', 'TOEFL', 'Project A', 'Company X'),
-  (2, 3.5, 'JavaScript, C++', 'IELTS', 'Project B', 'Company Y'),
-  (3, 3.9, 'Ruby, PHP', 'TOEIC', 'Project C', 'Company Z'),
-  (4, 3.7, 'Java, C#', 'TOEFL', 'Project D', 'Company A'),
-  (5, 3.9, 'Python, JavaScript', 'IELTS', 'Project E', 'Company B'),
-  (6, 3.6, 'C++, Ruby', 'TOEIC', 'Project F', 'Company C'),
-  (7, 3.5, 'Python, Java', 'TOEFL', 'Project G', 'Company D'),
-  (8, 3.8, 'JavaScript, C++', 'IELTS', 'Project H', 'Company E'),
-  (9, 3.9, 'Ruby, PHP', 'TOEIC', 'Project I', 'Company F');
-
--- Insert random data into company table
-INSERT INTO company (name, industry, location, website, contact_email, description)
-VALUES
-  ('Company X', 'Technology', 'New York', 'companyx.com', 'info@companyx.com', 'Company X is a leading technology company specializing in software development.'),
-  ('Company Y', 'Finance', 'London', 'companyy.com', 'info@companyy.com', 'Company Y is a financial institution providing investment and banking services.'),
-  ('Company Z', 'E-commerce', 'San Francisco', 'companyz.com', 'info@companyz.com', 'Company Z is an online marketplace offering a wide range of products.'),
-  ('Company A', 'Technology', 'San Francisco', 'companya.com', 'info@companya.com', 'Company A is a software development company specializing in AI and machine learning.'),
-  ('Company B', 'E-commerce', 'New York', 'companyb.com', 'info@companyb.com', 'Company B is an online marketplace providing a platform for buying and selling various products.'),
-  ('Company C', 'Finance', 'London', 'companyc.com', 'info@companyc.com', 'Company C is a financial institution offering banking and investment services.'),
-  ('Company D', 'Technology', 'San Francisco', 'companyd.com', 'info@companyd.com', 'Company D is a software development company specializing in web and mobile applications.'),
-  ('Company E', 'Finance', 'New York', 'companye.com', 'info@companye.com', 'Company E is an investment firm providing financial advisory services.'),
-  ('Company F', 'E-commerce', 'London', 'companyf.com', 'info@companyf.com', 'Company F is an online retailer offering a wide range of products.');
-
--- Insert random data into position table
-INSERT INTO position (company_id, title, description, is_internship)
-VALUES
-  (1, 'Software Engineer', 'Responsible for developing and maintaining software applications.', false),
-  (2, 'Financial Analyst', 'Conduct financial analysis and provide recommendations for investment strategies.', false),
-  (3, 'Frontend Developer Intern', 'Assist in developing user interfaces for the company website.', true),
-  (1, 'Full Stack Developer', 'Responsible for designing and implementing end-to-end solutions for web applications.', false),
-  (2, 'Product Manager', 'Manage product development lifecycle and collaborate with cross-functional teams.', false),
-  (3, 'Data Science Intern', 'Assist in analyzing and interpreting large datasets to extract meaningful insights.', true),
-  (1, 'Backend Developer', 'Responsible for developing server-side logic and APIs for web applications.', false),
-  (2, 'Financial Advisor', 'Provide financial planning and investment advice to clients.', false),
-  (3, 'UI/UX Design Intern', 'Assist in designing user interfaces and creating wireframes and prototypes.', true);
-
--- Insert random data into application table
-INSERT INTO application (student_id, position_id, application_date)
-VALUES
-  (1, 1, '2023-07-01'),
-  (2, 2, '2023-07-02'),
-  (3, 3, '2023-07-03'),
-  (4, 1, '2023-07-04'),
-  (5, 2, '2023-07-05'),
-  (6, 3, '2023-07-06'),
-  (7, 1, '2023-07-07'),
-  (8, 2, '2023-07-08'),
-  (9, 3, '2023-07-09');
-
--- Verify the data
-SELECT * FROM inform;
-SELECT * FROM achievement;
-SELECT * FROM company;
-SELECT * FROM position;
-SELECT * FROM application;
-
-
-
-
-
-
+(20180001, 3.5, 'Java', 'TOEFL', 'Aurora Rising', 'XYZ Company'),
+(20180002, 3.2, 'Python', 'IELTS', 'Stellar Fusion', 'ABC Corporation'),
+(20180003, 3.8, 'C++', 'TOEIC', 'Moonlight Serenade', 'DEF Inc.'),
+(20190001, 3.6, 'Java', 'TOEFL', 'Galaxy Quest', 'GHI Corporation'),
+(20190002, 3.9, 'Python', 'IELTS', 'Infinite Horizons', 'JKL Company'),
+(20220001, 3.7, 'C++', 'TOEIC', 'Quantum Nexus', 'MNO Corporation'),
+(20220002, 3.4, 'Java', 'TOEFL', 'Solaris Odyssey', 'PQR Inc.'),
+(20230001, 3.1, 'Python', 'IELTS', 'Nebula Chronicles', 'STU Corporation'),
+(20230002, 3.9, 'C++', 'TOEIC', 'Celestial Symphony', 'VWX Company'),
+(20230003, 3.8, 'Java', 'TOEFL', 'Project Epsilon', 'YZA Corporation'),
+(20220004, 3.2, 'Python', 'IELTS', 'Genesis Frontier', 'BCD Inc.'),
+(20230005, 3.6, 'C++', 'TOEIC', 'Lunar Mirage', 'EFG Corporation'),
+(20230006, 3.7, 'Java', 'TOEFL', 'Astral Enigma', 'HIJ Company'),
+(20210007, 3.5, 'Python', 'IELTS', 'Nova Vanguard', 'KLM Corporation'),
+(20220008, 3.9, 'C++', 'TOEIC', 'Orions Legacy', 'PQR Inc.'),
+(20230009, 3.6, 'Java', 'TOEFL', 'Comet Catalyst', 'STU Corporation'),
+(20230010, 3.4, 'Python', 'IELTS', 'Solstice Nexus', 'VWX Company'),
+(20200011, 3.8, 'C++', 'TOEIC', 'Cosmic Dreamers', 'YZA Corporation'),
+(20200002, 3.2, 'Java', 'TOEFL', 'Stardust Symphony', 'BCD Inc.');
